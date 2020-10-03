4fc1b9fa3830bdf3e444576695baeab8d052931b
Istiak-Tushar42 <istiak.tushar.it@gmail.com>
2020-10-04 03:50:08 +0600

    Header Finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,16 @@
-$('#slick').ddslick({
-    defaultSelectedIndex:2,
+/*
+** ddSlick PLUGIN
+*/
+// $('#slick').ddslick({
+//     defaultSelectedIndex:2,
+// });
+// ddSlick PLUGIN END
+
+
+/*
+** niceSelect PLUGIN
+*/
+$(document).ready(function() {
+    $('select').niceSelect();
 });
+// niceSelect PLUGIN END
